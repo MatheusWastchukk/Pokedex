cba7186a4f61c994d994e99477e3eef121bd63f9
matheuswastchukk <matheuswastchuk@gmail.com>
2024-01-23 21:42:09 -0300

    Adicionando  a página de detalhes

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,7 @@ let offset = 0;
 
 function convertPokemonToLi(pokemon) {
     return `
+    
         <li class="pokemon ${pokemon.type}">
             <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
@@ -19,6 +20,7 @@ function convertPokemonToLi(pokemon) {
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
             </div>
+            <a href="detalhes.html" class="details">Ver Mais</a>
         </li>
     `
 }
